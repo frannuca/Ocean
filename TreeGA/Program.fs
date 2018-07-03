1705d9ff275e06f0c53532513fd6dfb0a0ae4e55
frannuca <frannuca@gmail.com>
2018-07-03 18:15:19 +0200

    test GA function

--- TreeGA/Program.fs
+++ TreeGA/Program.fs
@@ -2,6 +2,7 @@
 open GA
 
 
+
 // Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
@@ -10,17 +11,19 @@ let main argv =
     printfn "%A" argv
 
     
-    let chromo = (new ChromosomeBuilder()).WithParameter(0.0,100.0,10,None)
-                                          .WithParameter(0.0,100.0,10,None)
-                                          .WithParameter(0.0,100.0,10,None)                                       
+    let chromo = (new ChromosomeBuilder()).WithParameter(-5.12,5.12,12,None)
+                                          .WithParameter(-5.12,5.12,12,None)
+                                          .WithParameter(-5.12,5.12,12,None)                                       
                                           .Build()
 
-    let fitness (x:float[]) = ((x.[0]-12.5)**2.0+(x.[1]-25.5)**2.0+(x.[2]-67.5)**2.0+1e-6)
-    let treeGA = new TreeGA(100,2,0.01,fitness,100.0,0.9,1.0,chromo)
+    let fitness (x:float[]) = 10.0 + ([0 .. x.Length-1] |> Seq.map(fun i -> x.[i]**2.0- 10.0 * System.Math.Cos(2.0* System.Math.PI*x.[i])) |> Seq.sum)
+    
+    let treeGA = new TreeGA(100,10,0.01,fitness,5000.0,0.97,0.5,chromo)
 
     let fitnessvec = [|0 .. 100|] |> Array.map(fun n -> treeGA.Next()) |> Array.ofSeq
 
     printfn "%A" (treeGA.GetHistoryBest())
-    
+       
+
         
     0 // return an integer exit code
